5d4f6bae03ca7f260a3ccdc7023311583a841351
SavchenkovAndrey <sannndvik16@gmail.com>
2023-04-24 18:58:32 +0200

    added deployHook

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+        "https://api.vercel.com/v1/integrations/deploy/prj_At3GWj1LW2lfYdfs1lnaCICPhmTy/VvNf14JVad",
       apiToken: "v8VBQEaxcPAcp8SvwjZjEnzS",
       appFilter: "chat-strapi",
       teamFilter: "chat-strapi",
